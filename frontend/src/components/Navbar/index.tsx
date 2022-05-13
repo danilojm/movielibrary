0c3866259aed473efa148eefb9549ed1195be961
DaniloJM <ejonhson@gmail.com>
2022-05-13 18:58:24 -0300

    Navbar

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,22 @@
+import { ReactComponent as GithubIcon } from "assets/img/gi_thub.svg";
+import './styles.css';
+
+function Navbar() {
+  return (
+    <header>
+      <nav className="container">
+        <div className="mlibrary-nav-content">
+          <h1 className="mlibrary-contact-title">Movies Library</h1>
+          <a href="https://github.com/danilojm/movielibrary">
+            <div className="mlibrary-contact-container">
+              <GithubIcon />
+              <p className="mlibrary-contact-link">/danilojm</p>
+            </div>
+          </a>
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default Navbar;
